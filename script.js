8d940c5c330dae19e434aa6fa36296a973ceaf45
Infrodri <joserodrigoriosa@gmail.com>
2024-09-12 21:55:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ let currentInput = '';
 let operator = '';
 let previousInput = '';
 
+function playSound(soundFile) {
+    const audio = new Audio(soundFile);
+    audio.play();
+}
+
 function addNumber(number) {
+    playSound(`sounds/${number}.mp3`); // Reproduce sonido del número
     currentInput += number;
     document.getElementById('result').value = currentInput;
 }
 
 function operate(op) {
+    playSound(`sounds/${op}.mp3`); // Reproduce sonido de la operación
     previousInput = currentInput;
     operator = op;
     currentInput = '';
@@ -30,6 +37,7 @@ function calculate() {
                 result = parseFloat(previousInput) / parseFloat(currentInput);
                 break;
         }
+        playSound('sounds/equals.mp3'); // Reproduce sonido al calcular
         document.getElementById('result').value = result;
         currentInput = result.toString();
         operator = '';
@@ -38,8 +46,10 @@ function calculate() {
 }
 
 function clearResult() {
+    playSound('sounds/clear.mp3'); // Reproduce sonido al limpiar
     currentInput = '';
     previousInput = '';
     operator = '';
     document.getElementById('result').value = '';
 }
+
